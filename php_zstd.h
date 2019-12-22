af00f948be422e77d0b94dc93d4b0f16ea1fb027
kj <kjclev@gmail.com>
2019-12-23 08:08:33 +0900

    Updated version to 0.7.6

--- php_zstd.h
+++ php_zstd.h
@@ -24,7 +24,7 @@
 #ifndef PHP_ZSTD_H
 #define PHP_ZSTD_H
 
-#define PHP_ZSTD_EXT_VERSION "0.7.5"
+#define PHP_ZSTD_EXT_VERSION "0.7.6"
 #define PHP_ZSTD_NS "Zstd"
 
 extern zend_module_entry zstd_module_entry;
