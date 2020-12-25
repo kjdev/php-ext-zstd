28644c5986b204f55e38e08441c5d06d76047044
kjclev <kjclev@gmail.com>
2020-12-25 10:42:56 +0900

    Added GitHub Actions

---
+++ .github/workflows/test.ps1
@@ -0,0 +1,9 @@
+$ErrorActionPreference = "Stop"
+
+$env:PATH = "C:\php\devel;C:\php\bin;C:\php\deps\bin;$env:PATH"
+
+$env:TEST_PHP_EXECUTABLE = "C:\php\bin\php.exe"
+& $env:TEST_PHP_EXECUTABLE 'run-tests.php' --show-diff tests
+if (-not $?) {
+    throw "testing failed with errorlevel $LastExitCode"
+}
