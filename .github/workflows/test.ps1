7e23e19c8e39ed095f4f9883ed4b5a474650dc85
kjdev <kjclev@gmail.com>
2024-10-23 09:35:48 +0900

    chore(ci): add color option and change artifacts output conditions during windows workflow

--- .github/workflows/test.ps1
+++ .github/workflows/test.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 $env:PATH = "C:\php\devel;C:\php\bin;C:\php\deps\bin;$env:PATH"
 
 $env:TEST_PHP_EXECUTABLE = "C:\php\bin\php.exe"
-& $env:TEST_PHP_EXECUTABLE 'run-tests.php' --show-diff tests
+& $env:TEST_PHP_EXECUTABLE 'run-tests.php' --color --show-diff tests
 if (-not $?) {
     throw "testing failed with errorlevel $LastExitCode"
 }
