ebd5372f1d464167f2a7743da3d1ac3c552f10d7
kjdev <kjclev@gmail.com>
2024-11-06 06:53:50 +0900

    upgrade: version to 0.14.0

--- php_zstd.h
+++ php_zstd.h
@@ -24,7 +24,7 @@
 #ifndef PHP_ZSTD_H
 #define PHP_ZSTD_H
 
-#define PHP_ZSTD_VERSION "0.13.3"
+#define PHP_ZSTD_VERSION "0.14.0"
 #define PHP_ZSTD_NS "Zstd"
 
 extern zend_module_entry zstd_module_entry;
