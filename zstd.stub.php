cef80f085f5ed346c64144950464759046b771e0
Timm Friebe <thekid@thekid.de>
2025-04-27 11:17:13 +0200

    ZstdContext only exists in global namespace

--- zstd.stub.php
+++ zstd.stub.php
@@ -2,7 +2,7 @@
 
 namespace {
 
-  class ZstdContext {}
+  final class ZstdContext {}
 
   function zstd_compress(string $data, int $level = 3): string|false {}
 
@@ -12,7 +12,7 @@ namespace {
 
   function zstd_uncompress_dict(string $data, string $dict): string|false {}
 
-  function zstd_compress_init(int $level= 3): ZstdContext|false {}
+  function zstd_compress_init(int $level = 3): ZstdContext|false {}
 
   function zstd_compress_add(ZstdContext $context, string $data, bool $end = false): string|false {}
 
@@ -24,8 +24,6 @@ namespace {
 
 namespace Zstd {
 
-  class ZstdContext {}
-
   function compress(string $data, int $level = 3): string|false {}
 
   function uncompress(string $data): string|false {}
@@ -34,12 +32,12 @@ namespace Zstd {
 
   function uncompress_dict(string $data, string $dict): string|false {}
 
-  function compress_init(int $level= 3): ZstdContext|false {}
+  function compress_init(int $level = 3): \ZstdContext|false {}
 
-  function compress_add(ZstdContext $context, string $data, bool $end = false): string|false {}
+  function compress_add(\ZstdContext $context, string $data, bool $end = false): string|false {}
 
-  function uncompress_init(): ZstdContext|false {}
+  function uncompress_init(): \ZstdContext|false {}
 
-  function uncompress_add(ZstdContext $context, string $data): string|false {}
+  function uncompress_add(\ZstdContext $context, string $data): string|false {}
 
 }
